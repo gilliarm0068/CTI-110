cf44575e51e6dacdb7cac41648ca141ed5e09034
gilliarm0068 <gilliarm0068@student.faytechcc.edu>
2026-09-11 14:23:31 -0400

    Add files via upload

---
+++ P1LAB1_GilliardMackenzie.py
@@ -0,0 +1,9 @@
+# Mackenzie Gilliard
+# 09/11/2026
+# P1LAB1
+# This program asks the user for their first and last name and prints a welcome message.
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Welcome,", first_name, last_name)
